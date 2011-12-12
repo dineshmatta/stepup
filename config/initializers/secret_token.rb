9034e6984337bb983b22604e9b3a9f8c58755995
Dinesh <dineshdm7@gmail.com>
2011-12-12 22:21:42 +0530

    Initialize the repository with first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StepUp::Application.config.secret_token = '3a42926904ed88c0b2696285141bed3561c0bbc43e17264ec024e88d2f13ec9fca444293fdaddff1cd02fcce504e761e20d50b9e1edc0ea13737fc1776009f2d'
